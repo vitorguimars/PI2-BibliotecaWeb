fa3063af3f0088fd7699dd650a410c6a16e7c021
Your Namevitorguimars <vitorguima.rs@bol.com.br>
2015-09-30 11:17:35 -0300

    Inicio Projeto

---
+++ BibliotecaWeb/src/java/Bean/ClienteBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import Model.Cliente;
+/**
+ *
+ * @author vitor
+ */
+@ManagedBean
+@RequestScoped
+public class ClienteBean {
+
+    /**
+     * Creates a new instance of ClienteBean
+     */
+    public ClienteBean() {
+    }
+    
+}
---
+++ BibliotecaWeb/src/java/Bean/RetiradaDevolucaoBean.java
@@ -0,0 +1,21 @@
+
+package Bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import Model.RetiradaDevolucao;
+/**
+ *
+ * @author vitor
+ */
+@ManagedBean
+@RequestScoped
+public class RetiradaDevolucaoBean {
+
+    /**
+     * Creates a new instance of RetiradaDevolucaoBean
+     */
+    public RetiradaDevolucaoBean() {
+    }
+    
+}
