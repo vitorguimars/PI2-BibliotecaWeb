9432db0214d333bfaf378e9cbe86ecf79bf48e07
Ravelli <Ravelli@Ravelli-PC>
2015-10-06 22:16:32 -0300

    Incluidos Classes e Managed Beans dos Livros, e correções diversas.

---
+++ BibliotecaWeb/src/java/bean/LivroMB.java
@@ -0,0 +1,71 @@
+
+package bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import model.Livro;
+
+/**
+ *
+ * @author vitor
+ */
+@ManagedBean(eager = true)
+@ApplicationScoped //Application, pois os usuários cadastrados deverão permanecer mesmo se fizer logout.
+public class LivroMB {
+
+    //CRUD
+    private List<Livro> listaLivros;
+    private Livro livroSelecionado;
+
+    public LivroMB() {
+        livroSelecionado = new Livro();
+        listaLivros = new ArrayList<Livro>();
+        listaLivros.add(new Livro(1111, "Brida", "Paulo Coelho", "Abril", 1994, 5));
+    }
+
+    public Livro getLivroSelecionado() {
+        return livroSelecionado;
+    }
+
+    public void setLivroSelecionado(Livro livroSelecionado) {
+        this.livroSelecionado = livroSelecionado;
+    }
+
+    public List<Livro> getListaLivros() {
+        return listaLivros;
+    }
+
+    public void setListaLivros(List<Livro> listaLivros) {
+        this.listaLivros = listaLivros;
+    }
+    
+
+    public String novoLivro(){
+        livroSelecionado=new Livro();
+        return("/livro3/cadastro?faces-redirect=true");
+    }
+
+    public String adicionarLivro()
+    {
+        listaLivros.add(livroSelecionado);
+        return(this.novoLivro());
+        
+    }
+
+    public String editarLivro(Livro u){
+        livroSelecionado = u;
+        return("/livro3/edicao?faces-redirect=true");
+    }
+    
+    public String atualizarLivro()
+    {
+        return("/livro3/index?faces-redirect=true");
+    }
+
+    public void removerLivro(Livro livro){
+        listaLivros.remove(livro);
+    }
+  
+}
---
+++ BibliotecaWeb/src/java/model/RetiradaDevolucao.java
@@ -0,0 +1,60 @@
+
+package model;
+
+import java.text.SimpleDateFormat;
+
+public class RetiradaDevolucao {
+    
+    private String dataRetirada;
+    private String dataParaDevolucao;
+    private Livro livro;
+    private Usuario usuario;
+    
+    public RetiradaDevolucao(Usuario usuario, Livro livro, String dataRetirada, String dataParaDevolucao){
+        this.dataRetirada = dataRetirada; 
+        this.dataParaDevolucao = dataParaDevolucao;
+        this.usuario = usuario;
+        this.livro = livro;
+    }
+
+    RetiradaDevolucao() {
+    }
+    
+    public void setDataRetirada(String dataRetirada){
+        
+        this.dataRetirada = dataRetirada;
+    }
+    
+    public String getDataRetirada(){
+        return dataRetirada;
+    }
+    
+    public void setDataParaDevolucao(String dataDevolucao){
+        
+        this.dataParaDevolucao = dataDevolucao;
+    }
+    
+    public String getDataParaDevolucao(){
+        return dataParaDevolucao;
+    }
+    
+    public void setUsuario(Usuario usuario){
+        
+        this.usuario = usuario;
+    }
+    
+    public Usuario getUsuario(){
+        return usuario;
+    }
+    
+    public void setLivro(Livro livro){
+        
+        this.livro = livro;
+    }
+    
+    public Livro getLivro(){
+        return livro;
+    }
+   
+}
+
